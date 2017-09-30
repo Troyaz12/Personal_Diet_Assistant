7b55f1370ae9fa55d98663142d08a67217a6a53e
Troy Gugler <guglert80@gmail.com>
2017-09-30 13:25:30 -0700

    feat: Add navigation bar

---
+++ app/src/main/java/tegprogramming/com/personal_diet_assistant/MainActivity.java
@@ -0,0 +1,86 @@
+package tegprogramming.com.personal_diet_assistant;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import static tegprogramming.com.personal_diet_assistant.R.id.drawer;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initNavigationDrawer();
+
+
+    }
+
+
+
+    private void initNavigationDrawer() {
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                switch (id){
+                    case R.id.mealsTab:
+
+
+
+                        break;
+                    case R.id.ingredientsTab:
+
+                        break;
+                    case R.id.dishTab:
+                        break;
+                }
+                return true;
+            }
+        });
+
+        drawerLayout = (DrawerLayout)findViewById(drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
+
+
+
+
+
+    }
+
+
+
+}
